ca6b65b8df733bb268ab0e6ac76b96bef0798479
GoruIV <149746335+GoruIV@users.noreply.github.com>
2024-12-28 23:15:51 +0700

    Update VipList.h

--- VipList.h
+++ VipList.h
@@ -1,7 +1,6 @@
 e3d891079e4e59a3 //hmzforevergg
 ae2570d4a849dab8 //Flocon
 1428db6b25d23fac //FloConVM
-49cb5b2f13d08cf6 //Goru
 4ac5ba42d6093aaf //Micka 1
 95cd98d46442fe54 //Micka 2
 7fd5b94f694d4f2d //Epic Shade
