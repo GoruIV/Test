7f6eb57f957c0d2cd5c400751908bbb97afbd236
GoruIV <149746335+GoruIV@users.noreply.github.com>
2025-08-11 02:49:21 +0700

    Edit line: 8255b12a2bba38899f5cb21edf4a549c //_arturus -> 9d2f7aeacf24380da96f37728e0c1f99 //_arturus in VipList.h

--- VipList.h
+++ VipList.h
@@ -1134,7 +1134,7 @@ ed1748ac317d3a0b993b85a7bc66d1f7 //Kontoruツ
 a39860b5674d369db378558a8b5b339f //benik13
 bd174dd0bba2324089b68398bad02d07 //dynortyce
 dcc346ca69723acc8edbd1ff9ee4eb5d //#jedaiwun
-8255b12a2bba38899f5cb21edf4a549c //_arturus
+9d2f7aeacf24380da96f37728e0c1f99 //_arturus
 29bbc7a209e638ae8760b78df4088b64 //janeny1992
 33f3689fc66d35fd9b6192786bfebb77 //Thuyet-M8
 e87320121e8734018bf7773ff59c7c29 //nicoyteam
