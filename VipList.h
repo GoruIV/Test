3272c0d198de247fc869dd9de9c4d669d9b7475f
GoruIV <149746335+GoruIV@users.noreply.github.com>
2025-10-02 01:41:35 +0700

    Add ID: 588f98d965963bca8544142130a684e7 //ric2322 to VipList.h

--- VipList.h
+++ VipList.h
@@ -2620,3 +2620,4 @@ d81d1444787a3a58ac2b11bbf7151ff3 //uhh_for_what
 910ae64bc60938c79fc3f8f94ec35dee //lipe08976
 bf726d36b20f3b2ea01da9b7b0811e11 //.bongjongo
 fc9cffc7af6a37c291dded2a9d38bdd7 //jovenaprendiz
+588f98d965963bca8544142130a684e7 //ric2322
