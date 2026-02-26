dc255b18959bfaf7373fcda6e4e5850faf1a55ec
FloConHMZ <floconyt@gmail.com>
2026-02-26 19:56:53 +0100

    Remove ID for ibby0000 (slot 2)

--- VipList.h
+++ VipList.h
@@ -801,7 +801,6 @@ dcabad8c432a3e448cab34fc2102c9d8 //carteldesanta127
 c939398fbca837cabf19fa8daf13cb16 //laurent4166
 44030ae4aa6e3084bc41b6d87fcb9853 //ba_ba.5678
 4cc9537c53e3337ba38b07b66daf47de //karolineyg
-229de8d62e863314a21bc435ad6c3414 //ibby0000
 a8171726865f3dcb9d85ef488aff50b9 //salvatore_fraquim
 0ddca0d16e5637f186262f2e0c239fa5 //akaza03662
 d8cfec29024b38279707f32293c91fad //jackinmihoff
