0b59f03ce8c0ad6d3e9ef934c994119577e074e2
FloConHMZ <floconyt@gmail.com>
2026-04-11 02:01:16 +0200

    Remove ID for mr.dumbledoore (slot 1)

--- VipList.h
+++ VipList.h
@@ -1504,7 +1504,6 @@ fb59ab239ade30e3b7c1f5e01c4bdb6f //datboihigh
 6cf4a06b2be7301ca2119fce9dcd00f2 //maybeyou.
 a4d39fa4d0f53fdba8390334e39f28ff //tipi.295
 2fa2ed3960c93ec18226e83f367e53fa //oblivion6660275
-f87ddd830cc53c7e808f588283f37f25 //mr.dumbledoore
 ef9c253188ab3c3cb394651d9b6ee54b //monostori.adam.r
 66bc8e9f478f37c4a2a279b5ac5224de //jovakingz_78175
 f8144ff7aaf03976a32e6f0775a66806 //josephxp659ps4
