0c80b55c896c872054b1fd2ce81d0f33516a9ddd
FloConHMZ <floconyt@gmail.com>
2026-07-03 21:04:21 +0200

    Remove ID for kamurastore (slot 1)

--- VipList.h
+++ VipList.h
@@ -2199,7 +2199,6 @@ a4674cac9d743a5c8a4646c05ecf8ccf //ameenji
 17cbb5c8f4eb315780dd89c4d4554fb7 //bryani6055
 205ccb9ec217320aa4f74adef3c4b35a //prokipo
 0e41f04832943353be36a16752cae246 //ferjunior7
-b87a95c30ebb3c5e98d94c9a882d5dd6 //kamurastore
 d19bfa50dde13edcb840d17940e583a3 //.kopter
 17b6606dd3023c3fa4909e7fbdfb339e //casmy__luvv ⏳
 1cf401f5463e3a069867ed55f7979b6b //mrcharlesir
