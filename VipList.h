d6ab05826c909e81d849cfa3d861c39942862fa6
FloConHMZ <floconyt@gmail.com>
2026-08-02 19:31:56 +0200

    Remove ID for shinseolhwa (slot 1)

--- VipList.h
+++ VipList.h
@@ -2461,5 +2461,4 @@ a275d3efc8c33ec08b664bfcf24eb5da //awitadecocox007
 b0f193ba513f3499ab66140f41128c49 //talhabinsohail
 6eb003fa36e230e99af9fa485f40becf //estmusic
 e61719aa7ac2312391ee1f722700bba1 //hadiyapiyush
-9d9adbabb5f238a9903032dc04d067c0 //shinseolhwa
 f9da07b16e8537329da0208b44baf262 //shinseolhwa
