ea77808e2a0d7e5d85a00dea3ce5401a26d18d84
FloConHMZ <floconyt@gmail.com>
2026-08-17 20:09:41 +0200

    Remove ID for vapo07018 (slot 1)

--- VipList.h
+++ VipList.h
@@ -2365,7 +2365,6 @@ b9c2a710c0f4384f80c987e813c2afcb //dragonlord_0101
 cd32564dd63a3a4b943f2a33c6fbca82 //emilromedias_91986
 b7f3c2bb480e3df8bd80afdd60227cc1 //jrdnsanjaya
 7e430cd6bb2e3d4fa8714a2221fa1429 //bellum4vem
-3830ba77c4e734ff9317c3078aa1d5d5 //vapo07018
 b32dbd854b1c43f3ba79e42653f1fc42 //jakethesbake
 515755677a1e388bab214aabcad3ad27 //jakethesbake
 d5c91d0ab6bb3acea37e34e751c63a03 //balerionthebig_.
