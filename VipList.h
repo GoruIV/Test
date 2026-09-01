6a4b91347e09d0e9c12d6cd48017fa77d9251abb
FloConHMZ <floconyt@gmail.com>
2026-09-01 12:31:17 +0200

    Remove ID for sisu_lumincenxe (slot 2)

--- VipList.h
+++ VipList.h
@@ -2667,7 +2667,6 @@ a2adb16a6ee031d88ff87febd3246138 //maryvilla0049
 291cd2257d6735a4bdc3caf048cd0bc2 //leocaradepeo
 3c12f7bcbb2a360d8d19b0f0ac7ab742 //skull06224
 a1166960a3e3362592e0c989d5e525b5 //phantasys
-1216bdbd30ce36b9935ceab0a00dcd84 //sisu_lumincenxe
 a16ab61feab8380eac826906cc0d08c7 //marceallende
 8b06515a6c76387cb0bea569eb1b8b40 //ka1u.
 31791b40fded3f51aecf5397a928e59a //gari6752
