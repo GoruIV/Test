74b8725649bd6e7871a489d3599c6c47f7db8075
FloConHMZ <floconyt@gmail.com>
2026-09-01 12:15:09 +0200

    Replace ID for empire.0001 (slot 2)

--- VipList.h
+++ VipList.h
@@ -777,7 +777,7 @@ dcbf79d6d2ba317fb4307ea5ade4b84b //mentaluk.
 4548948d70ed35e0a82f3965ca115c75 //ov00.
 60f3ef80a4d037f08ede6ccef5524c5f //kelve_8
 ccac19b4235c3742b8ce8e6fdcf3c0dd //weqspeer
-d9ce79b5648d3e85a47ba32f7232055f //empire.0001
+979c6cc8f2b032e4bfb7c5f0f0a01f7b //empire.0001
 74204ec950633b7db4fbc233a26a2a58 //wamiyu_gaming
 30aa0357722d38b598c884e5993f0f26 //ericdrone_mys
 5612f2ebf24232c8ad3a9d19a209a830 //jaydipkumar143
