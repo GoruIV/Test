5d2f5fef455372c85980587ad01207f455bc2603
FloConHMZ <floconyt@gmail.com>
2026-09-04 18:24:31 +0200

    Remove ID for kyouya_02 (slot 2)

--- VipList.h
+++ VipList.h
@@ -2728,7 +2728,6 @@ bd4aca8804cd3362a5eb17cedac28911 //yibibi._56821
 e3e2fa8f4959371f9699225666d1cdf3 //numetalblud
 b6b7c52a13e740c5acebf7c0523909ed //tyelovestomch6
 6f719b355a113f6298b3e67c9361b5ae //tyelovestomch6
-24eadf0d829a31f58e5a5c83def29cae //kyouya_02
 e94b7ffae8e63683bc46e8cb28842a1a //redrodry.
 44d0925c63de39298beb59bc62cc5111 //amz_apollo
 e23fd1843c093ff2b69a2a984a65ca37 //corvalovell
