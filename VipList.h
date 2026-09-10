d7e7e9570dd8046e17ab5dc8a8e6dd20800ee69d
FloConHMZ <floconyt@gmail.com>
2026-09-10 02:52:49 +0200

    Remove ID for maksimus7647 (slot 1)

--- VipList.h
+++ VipList.h
@@ -2880,4 +2880,3 @@ d915ce6ace943d0c850441f928e145ee //shinseolhwa
 95bf2c2b545133bf8fc1fdf13d53f6df //ryzeupbabyyy
 45f42d5623b4316fbdde671384aeb661 //vulturebrawl
 ccb75d00bede3e8bb5828dfbe0b8eec4 //inuyasha01035
-7d2cacb29a8f3248ae09674ec2157cf5 //maksimus7647
