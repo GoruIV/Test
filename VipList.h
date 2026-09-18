81caf885ac55ad4851e2c3075fd471881a00c30d
FloConHMZ <floconyt@gmail.com>
2026-09-19 01:27:13 +0200

    Remove ID for motyemody (slot 1)

--- VipList.h
+++ VipList.h
@@ -2940,7 +2940,6 @@ d32ad9b3482b3501bbeaf5c84f0dccb9 //simy9627
 dc664284e226315d8f75bd9fff09724b //sarfraz_khowaja
 3fd9c509fed23c64891c37dfedfd8286 //babayaga095
 8f6f3d03c8433fed98e8705b9672d354 //night0042
-238dd37cae5532db8eb91d02cf4a6e14 //motyemody
 a671e31fdce83a33bf6ac65c4388b91e //xxmaksimxx5
 485a648f978c361487715930924f2d2f //tuga57.
 716506e1cae831adbb3e4cb66cf88ad8 //im_shichibukai
