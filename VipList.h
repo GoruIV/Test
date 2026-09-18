3810ba337e94a17f65fd0021cfe1e35f494445a7
FloConHMZ <floconyt@gmail.com>
2026-09-18 08:08:23 +0200

    Remove ID for seekeedm96 (slot 1)

--- VipList.h
+++ VipList.h
@@ -1563,7 +1563,6 @@ d0be72d2d8c03e81a62fbe7cc904b666 //fazgulp
 e5d4a36fd2f53d819fa8c16eb8017d38 //manuelmailo357
 10ebcda4fc71397c9cc9b3ef7133f801 //tempestblow
 305eb6d25d9139e89f2dbd3f1620a0a0 //carllosboni4501
-de97236ceaed3db398fb2b8c5a70dd1c //seekeedm96
 553365bd53d23d4ca047b0fa041ba286 //fazgulp
 6e5056d94c0d3bb999b680330a0d372e //lmleo
 0333efa2a18c358790713cdd9d17c1de //tempestblow
