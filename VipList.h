ca3917763e8380341444056971ae092d4ebf81f8
FloConHMZ <floconyt@gmail.com>
2026-09-20 09:08:09 +0200

    Remove ID for blindguardian1000 (slot 2)

--- VipList.h
+++ VipList.h
@@ -2921,7 +2921,6 @@ d32ad9b3482b3501bbeaf5c84f0dccb9 //simy9627
 91d8a1f4e12435aca204995636d98f6d //sisu_lumincenxe
 72fd1da5b827396cbf7b1a044c15589d //angrybird_.
 41344c90545b3299acb4beed4cb8a094 //nhatruuc.24
-8d7cf82671bb3ffc91096b05cfef2d8b //blindguardian1000
 5219c756d39234b5a36b6e8531208ccb //mskent.
 dc664284e226315d8f75bd9fff09724b //sarfraz_khowaja
 3fd9c509fed23c64891c37dfedfd8286 //babayaga095
