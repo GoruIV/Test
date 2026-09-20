0e049e3389792da2d6faae4595d0e2c9b9cda3f1
FloConHMZ <floconyt@gmail.com>
2026-09-20 12:14:58 +0200

    Remove ID for lenavaronin (slot 1)

--- VipList.h
+++ VipList.h
@@ -2990,5 +2990,4 @@ a81d5fd634ad3908abe1eb6e18092c05 //ask.games
 a3149e419123374887424b10ac3ca1fd //_epee33
 c668c893dadd34a3941ff327bff397cf //blindguardian1000
 a3c11287c1613ae8a7990db821f1f451 //redl1ne_z
-4b863dff357a322b8daa3403298dbc6d //lenavaronin
 71aa4ffba81b32e1bae09933a2383ebb //lenavaronin
